2fe1bd7edaf9e6d952610a49545e2d62591546f0
Eric Steinberg <eric@MASTER.MSHOME>
2016-08-27 19:10:12 -0500

    Initial Commit

---
+++ assets/javascript/app.js
@@ -0,0 +1,102 @@
+/*
+ *STATIC VARIABLES
+ */
+ var DEFAULT_LIST=["South Park","Bob\'s Burgers", "Archer","Futurama", "American Dad","Family Guy", "Venture Brothers", "Rick and Morty", "The Simpsons", "Bojack Horseman", 
+ "Daria", "Robot Chicken"];
+
+
+
+
+$(document).ready(function(){
+
+	for(var i=0;i<DEFAULT_LIST.length;++i){
+
+		createButton(DEFAULT_LIST[i]);
+	}
+
+});
+
+
+$(document).on("click", ".buttonSearch", function(){
+
+	runAPI($(this).data("name"));
+
+});
+
+$("#addButton").on("click", function(){
+	createButton($("#addCategoryBox").val());
+
+});
+
+
+$(document).on("click", ".imageClick", function(){
+	var animate = $(this).data("animate");
+	var static = $(this).data("static");
+	console.log(animate);
+
+	if($(this).attr("state") === "static"){
+		$(this).attr("src", animate);
+		$(this).attr("state", "animate");
+
+	}
+	else{
+		$(this).attr("src", static);
+		$(this).attr("state", "static");
+	}
+
+});
+
+
+function createButton(name){
+
+	var singleButton = $("<button>");
+
+	singleButton.attr("data-name",name);
+	singleButton.attr("class","buttonSearch");
+	singleButton.html(name);
+	$("#buttonContainer").append(singleButton);
+}
+
+
+function runAPI(name){
+
+	var queryURL = "http://api.giphy.com/v1/gifs/search?q="+name+"&api_key=dc6zaTOxFJmzC";
+
+	$.ajax({url: queryURL, method: 'GET'})
+	 .done(function(response) {
+	 	$("#imageContainer").empty();
+
+	 	var imgArraylength;
+
+	 	//Check to see if object array length is no greater than 10 (per HW requirement)
+	 	if(response.data.length>=10){
+	 		imgArraylength =10;
+	 	}
+	 	else{
+	 		imgArraylength= response.data.length;
+	 	}
+
+	    for(var i=0;i<imgArraylength;++i){
+	    	createImgContainer(response.data[i]);
+	 	}
+	}); 
+
+}
+
+function createImgContainer(gifObject){
+	var imgContainer = $("<div>");
+	var imgText = $("<p>");
+	var img = $("<img>");
+
+	imgText.html(gifObject.rating);
+	img.attr("src", gifObject.images.fixed_width_still.url);
+	img.attr("data-static", gifObject.images.fixed_width_still.url);
+	img.attr("data-animate", gifObject.images.fixed_width.url);
+	img.attr("state", "static");
+	img.attr("class", "imageClick");
+
+	imgContainer.append(imgText,img);
+	$("#imageContainer").append(imgContainer);
+
+
+}
