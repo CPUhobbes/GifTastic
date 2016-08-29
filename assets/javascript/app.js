87c8052d65f116af08e0e9c6c38fc73e589321bf
Eric Steinberg <eric@MASTER.MSHOME>
2016-08-29 17:49:09 -0500

    Added more css, fixed changing background bug

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,13 +8,27 @@
 
 
 
-
+//On page load, display all defualt buttons
 $(document).ready(function(){
 	displayButtons();
 	
 });
 
+//If enter is pressed in the textbox, add that button
+$("input").keypress(function(event) {
+    if (event.which == 13) {
+        event.preventDefault();
+        addButton();
+    }
+});
 
+//If Add TV Show button is pressed, add that button
+$("#addButton").on("click", function(){
+	addButton();
+
+});
+
+//If a TV show button is clicked, get API results and display them
 $(document).on("click", ".buttonSearch", function(){
 
 	var tvshow = $(this).data("name");
@@ -23,41 +37,44 @@ $(document).on("click", ".buttonSearch", function(){
 
 });
 
-$("#addButton").on("click", function(){
-	TOPICS.push($("#addCategoryBox").val());
-	displayButtons();
-
-});
-
-
+//If a gif is clicked and not running, run the gif, else stop the gif
 $(document).on("click", ".imageClick", function(){
 	var animate = $(this).data("animate");
 	var static = $(this).data("static");
-	console.log(animate);
+	//console.log(animate);
 
-	if($(this).attr("state") === "static"){
+	if($(this).attr("data-state") === "static"){
 		$(this).attr("src", animate);
-		$(this).attr("state", "animate");
+		$(this).attr("data-state", "animate");
 
 	}
 	else{
 		$(this).attr("src", static);
-		$(this).attr("state", "static");
+		$(this).attr("data-state", "static");
 	}
 
 });
 
 
+//Add TV show to array and display buttons
+function addButton(){
+	TOPICS.push($("#addCategoryBox").val());
+	displayButtons();
+
+}
+
+//Create buttom for each element in the button array
 function createButton(name){
 
 	var singleButton = $("<button>");
 
 	singleButton.attr("data-name",name);
-	singleButton.attr("class","buttonSearch");
+	singleButton.attr("class","btn btn-primary buttonSearch");
 	singleButton.html(name);
 	$("#buttonContainer").append(singleButton);
 }
 
+//Display each element in the button array
 function displayButtons(){
 	$("#buttonContainer").empty();
 	for(var i=0;i<TOPICS.length;++i){
@@ -65,7 +82,7 @@ function displayButtons(){
 	}
 }
 
-
+//Run API call and get gif still image and actual gif.  Display gif still image
 function runAPI(name){
 
 	var queryURL = "http://api.giphy.com/v1/gifs/search?q="+name+"&api_key="+GIPHY_API_KEY;
@@ -75,7 +92,7 @@ function runAPI(name){
 	 	$("#imageContainer").empty();
 
 	 	var imgArraylength;
-	 	var multiplesFive=1;
+	 	var multiples=1;
 
 	 	//Check to see if object array length is no greater than 10 (per HW requirement)
 	 	if(response.data.length>=10){
@@ -88,25 +105,44 @@ function runAPI(name){
 	    for(var i=0;i<imgArraylength;++i){
 	    	//createImgContainer(response.data[i]);
 	    	if((i+1)%2===0){
-	    		multiplesFive+=1;
+	    		multiples+=1;
 	    	}
 	 	}
-	 	console.log(multiplesFive);
-	 	createImgContainers(response.data,multiplesFive,imgArraylength);
+	 	//console.log(multiples);
+	 	createImgContainers(response.data,multiples,imgArraylength);
 	}); 
 
 }
 
-function createImgContainers(gifObject,multiplesFive,imgArraylength){
+//Get background from tvmaze API and set background to image
+//If multiple shows have same name, undefined response will be returned (per API documentation)
+function tvBackground(tvshow){
+
+var queryURL = "http://api.tvmaze.com/singlesearch/shows?q="+tvshow;
+
+	$.ajax({url: queryURL, method: 'GET', 
+		success: function (response) {
+        	$("body").css({"background-image": "url("+response.image.original+")"});
+    	}, 
+    	//If cannot find show use default black image
+    	error: function (response){
+    		$("body").css({"background-image": "url(\"../images/bg_default.png\")"});
+    	}
+
+	});
+}
+
+//Create image containers for each gif
+function createImgContainers(gifObject,multiples,imgArraylength){
 	
 	var rowDivArray= new Array();
-	for(var i=0;i<multiplesFive;++i){
+	for(var i=0;i<multiples;++i){
 		var tempRow = $("<div>");
 		tempRow.attr("class","row");
 		rowDivArray.push(tempRow);
 	}
 
-	console.log(rowDivArray.length);
+	//console.log(rowDivArray.length);
 
 	var rowCounter=0;
 
@@ -121,56 +157,23 @@ function createImgContainers(gifObject,multiplesFive,imgArraylength){
 		var img = $("<img>");
 
 		imgContainer.attr("class","col-md-6");
+		imgText.attr("class", "imgText")
 		imgText.html("Rating:" +gifObject[i].rating.toUpperCase());
 		img.attr("src", gifObject[i].images.fixed_height_still.url);
 		img.attr("alt", "anImage");
 		img.attr("data-static", gifObject[i].images.fixed_height_still.url);
 		img.attr("data-animate", gifObject[i].images.fixed_height.url);
-		img.attr("state", "static");
+		img.attr("data-state", "static");
 		img.attr("class", "imageClick");
 
 		imgContainer.append(imgText,img);
 
 
-		console.log("item"+i+"  "+rowCounter);
+		//console.log("item"+i+"  "+rowCounter);
 		rowDivArray[rowCounter].append(imgContainer);
-
-		
-
-		
 
 	}
 	for(var i=0;i<rowDivArray.length;++i){
 		$("#imageContainer").append(rowDivArray[i]);
 	}
-
-
-
-	// var imgContainer = $("<div>");
-	// var imgText = $("<p>");
-	// var img = $("<img>");
-
-	// imgText.html(gifObject.rating);
-	// img.attr("src", gifObject.images.fixed_width_still.url);
-	// img.attr("data-static", gifObject.images.fixed_width_still.url);
-	// img.attr("data-animate", gifObject.images.fixed_width.url);
-	// img.attr("state", "static");
-	// img.attr("class", "imageClick");
-
-	// imgContainer.append(imgText,img);
-	// $("#imageContainer").append(imgContainer);
-
-
-}
-
-function tvBackground(tvshow){
-
-var queryURL = "http://api.tvmaze.com/singlesearch/shows?q="+tvshow;
-
-	$.ajax({url: queryURL, method: 'GET'
-
-})
-	 .done(function(response) {
-	 	$("body").css({"background-image": "url("+response.image.original+")"});
-	});
 }
